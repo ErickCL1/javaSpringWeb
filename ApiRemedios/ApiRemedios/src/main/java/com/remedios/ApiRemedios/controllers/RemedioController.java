809ef22fc3247b0190495ed739684442e64b6206
ErickCL1 <erickcordeiro23@gmail.com>
2023-10-15 19:43:21 -0300

    adicionamento dos drivers do banco de dados

--- ApiRemedios/ApiRemedios/src/main/java/com/remedios/ApiRemedios/controllers/RemedioController.java
+++ ApiRemedios/ApiRemedios/src/main/java/com/remedios/ApiRemedios/controllers/RemedioController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.remedios.ApiRemedios.remedio.DadosCadastroRemedios;
+
 @RestController
 @RequestMapping("/remedios")
 public class RemedioController {
 	
 	@PostMapping
-	public void cadastrar(@RequestBody String json) {
-		System.out.println(json);
+	public void cadastrar(@RequestBody DadosCadastroRemedios dados) {
+		System.out.println(dados);
 	}
 }
