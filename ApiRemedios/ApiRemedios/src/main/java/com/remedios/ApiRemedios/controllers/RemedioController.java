8c4d7dedf3a483a1c64b327d2388763db3a0d028
ErickCL1 <erickcordeiro23@gmail.com>
2023-10-21 16:33:44 -0300

    Implementação do verbo GET

--- ApiRemedios/ApiRemedios/src/main/java/com/remedios/ApiRemedios/controllers/RemedioController.java
+++ ApiRemedios/ApiRemedios/src/main/java/com/remedios/ApiRemedios/controllers/RemedioController.java
@@ -1,15 +1,20 @@
 package com.remedios.ApiRemedios.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.remedios.ApiRemedios.remedio.DadosCadastroRemedios;
+import com.remedios.ApiRemedios.remedio.DadosListagemRemedio;
 import com.remedios.ApiRemedios.remedio.Remedio;
 import com.remedios.ApiRemedios.remedio.RemedioRepository;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 @RestController
@@ -20,8 +25,15 @@ public class RemedioController {
 	private RemedioRepository repository;
 	
 	@PostMapping
+	@Transactional 
 	public void cadastrar(@RequestBody @Valid DadosCadastroRemedios dados) {
 		repository.save(new Remedio(dados));
+	}
+	
+	@GetMapping
+	public List<DadosListagemRemedio> Listar(){
+		return repository.findAll().stream().map(DadosListagemRemedio::new).toList();
+		
 	}
 	
 }
